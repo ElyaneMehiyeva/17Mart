2f1dd018677b337480e2eb1f8b9709acc7b08da7
Elyane <elyamehiyeva@gmail.com>
2022-03-17 08:21:02 +0400

    17Mart

---
+++ 17Mart/Group.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17Mart
+{
+    internal class Group
+    {
+        private string _groupno;
+        private int _studentLimit;
+        public string GroupNo
+        {
+            get { return this._groupno; }
+            set 
+            {
+                if (CheckCondition(value))
+                {
+                    this._groupno = value;
+                }
+            }
+        }
+        public int StudentLimit
+        {
+            get { return this._studentLimit; }
+            set 
+            {
+                if (value >= 0)
+                {
+                    this._studentLimit = value;
+                }
+            }
+        }
+        public bool CheckCondition(string text)
+        {
+            if(!string.IsNullOrWhiteSpace(text) && text.Length==5 && char.IsUpper(text[0]) && char.IsUpper(text[1]) && char.IsDigit(text[2]) && char.IsDigit(text[3]) && char.IsDigit(text[4]))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    
+}
---
+++ 17Mart/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _17Mart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uzunluq daxil edin :");
+            string numStr = Console.ReadLine();
+            int num;
+            while (!int.TryParse(numStr, out num))
+            {
+                Console.WriteLine("Uzunluq daxil edin (Eded):");
+                numStr = Console.ReadLine();
+            }
+            Group[] GroupArray = new Group[num];
+            
+            for (int i = 0; i < num; i++)
+            {
+                string groupNo;
+                int studentLimit;
+                string studentLimitStr;
+                Console.WriteLine($"{i+1}. Group No :");
+                groupNo = Console.ReadLine();
+                while (!CheckCondition(groupNo)) 
+                {
+                    Console.WriteLine($"{i + 1}. Group No Dogru Formatda Daxil Edin!");
+                    groupNo = Console.ReadLine();
+                }
+                Console.WriteLine($"{i+1}. Student Limit :");
+                studentLimitStr = Console.ReadLine();
+                int.TryParse(studentLimitStr, out studentLimit);
+                while (!int.TryParse(studentLimitStr, out studentLimit))
+                {
+                    Console.WriteLine($"{i + 1}. Student Limit + Eded Olmalidir :");
+                    studentLimitStr = Console.ReadLine();
+                    int.TryParse(studentLimitStr, out studentLimit);
+                }
+                Group group = new Group();
+                group.GroupNo = groupNo;
+                group.StudentLimit = studentLimit;
+                GroupArray[i] = group;
+            }
+            Console.WriteLine("Group Yaratma Isi Bitdi !");
+            foreach (Group item in GroupArray)
+            {
+                Console.WriteLine("Group No : {0} -*- Student Limit : {1}", item.GroupNo, item.StudentLimit);
+            }
+            Console.WriteLine("The End");
+        }
+        static bool CheckCondition(string text)
+        {
+            if (!string.IsNullOrWhiteSpace(text) && text.Length == 5 && char.IsUpper(text[0]) && char.IsUpper(text[1]) && char.IsDigit(text[2]) && char.IsDigit(text[3]) && char.IsDigit(text[4]))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
